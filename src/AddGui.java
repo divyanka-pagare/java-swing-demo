166f9ef4a9dc8fcc9e412030b2d87880084f1f2b
dell <pagaredivyanka@gmail.com>
2026-06-01 01:12:00 +0530

    addition

---
+++ src/AddGui.java
@@ -0,0 +1,34 @@
+package src;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+
+public class AddGui {
+    public static void main(String[] args) {
+        Addition obj = new Addition();
+    }
+}
+
+class Addition extends JFrame {
+    public Addition() {
+
+        JTextField text1 = new JTextField();
+        JTextField text2 = new JTextField();
+        JButton button = new JButton("OK");
+        JLabel Label = new JLabel("Result");
+
+        add(text1);
+        add(text2);
+        add(button);
+        add(Label);
+
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
