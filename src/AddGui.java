51e18e958533ebb77702e911602939b04a48f611
dell <pagaredivyanka@gmail.com>
2026-06-01 01:37:23 +0530

    use ActionListener

--- src/AddGui.java
+++ src/AddGui.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AddGui {
     public static void main(String[] args) {
@@ -12,23 +14,38 @@ public class AddGui {
     }
 }
 
-class Addition extends JFrame {
+class Addition extends JFrame implements ActionListener {
+
+    JTextField text1;
+    JTextField text2;
+    JButton button;
+    JLabel label;
+
     public Addition() {
 
-        JTextField text1 = new JTextField(20);
-        JTextField text2 = new JTextField(20);
-        JButton button = new JButton("OK");
-        JLabel Label = new JLabel("Result");
+        text1 = new JTextField(20);
+        text2 = new JTextField(20);
+        button = new JButton("OK");
+        label = new JLabel("Result");
 
         add(text1);
         add(text2);
         add(button);
-        add(Label);
+        add(label);
 
+        button.addActionListener(this); //ActionListerner is an Interface 
+        
         setLayout(new FlowLayout());
         setVisible(true);
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        int num1 = Integer.parseInt(text1.getText()); //converting string to integer , by using --> Integer.parseInt() method
+        int num2 = Integer.parseInt(text2.getText());
+        int result = num1 + num2;
+        label.setText("Result: " + result);
     }
 }
