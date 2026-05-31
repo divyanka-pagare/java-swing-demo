f491b4c4f03586ed815ef9687717e394ad3c633f
dell <pagaredivyanka@gmail.com>
2026-06-01 00:33:24 +0530

    JLabel

--- src/FirstGUI.java
+++ src/FirstGUI.java
@@ -1,16 +1,23 @@
 package src;
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
 public class FirstGUI {
     public static void main(String[] args) {
         Abc obj = new Abc();
-        obj.setVisible(true);
-        obj.setSize(400, 400);
+        
     }
 }
 
 class Abc extends JFrame {
     public Abc() {
 
+        JLabel label = new JLabel("Hello World!");
+        JLabel name = new JLabel("Hey, I am Divyanka");
+
+        add(label);
+        add(name);
+        
+        setVisible(true);
+        setSize(400, 400);
     }
 }
