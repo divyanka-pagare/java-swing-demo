b6c4e97c4d4e6f9d8877a490cc13f0d70ae6186a
dell <pagaredivyanka@gmail.com>
2026-06-01 00:46:47 +0530

    FlowLayout

--- src/FirstGUI.java
+++ src/FirstGUI.java
@@ -1,6 +1,7 @@
 package src;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.FlowLayout;
 public class FirstGUI {
     public static void main(String[] args) {
         Abc obj = new Abc();
@@ -8,9 +9,10 @@ public class FirstGUI {
     }
 }
 
-class Abc extends JFrame {
+class Abc extends JFrame { // CardLayout 
     public Abc() {
 
+        setLayout(new FlowLayout()); //flowLayout, borderLayout, gridLayout, Null
         JLabel label = new JLabel("Hello World!");
         JLabel name = new JLabel("Hey, I am Divyanka");
 
